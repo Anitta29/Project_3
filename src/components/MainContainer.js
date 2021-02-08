0731e34d5e421848ed764eb5d4244d9f428d6636
Alex Fernandes <alexfernands@outlook.com>
2021-02-08 15:37:20 -0500

    created components

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 import Container from "./Container";
 
-
-
-
 class realtorContainer extends Component {
+	
+	render() {
+		return (
+			<Container>
+				
+			</Container>
+		);
+	}
+}
+
+
 
 
 
