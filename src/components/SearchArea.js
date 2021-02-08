ab2d05106d8d2f644b8dc55b58c14721398e2d4b
Alex Fernandes <alexfernands@outlook.com>
2021-02-08 16:03:22 -0500

    created search field

--- src/components/SearchArea.js
+++ src/components/SearchArea.js
@@ -1,7 +1,86 @@
 import React from "react";
 
 function SearchForm(props) {
-	return <form></form>;
+	return (
+		<form>
+			<div className="formGroup">
+				<label htmlFor="search">Search:</label>
+				<select
+					name="state"
+					id="state"
+					onChange={props.handleInputChange}
+					value={props.state}
+					className="form-control"
+				>
+					<option value="">Select a State</option>
+					<option value="AL">Alabama</option>
+					<option value="AK">Alaska</option>
+					<option value="AZ">Arizona</option>
+					<option value="AR">Arkansas</option>
+					<option value="CA">California</option>
+					<option value="CO">Colorado</option>
+					<option value="CT">Connecticut</option>
+					<option value="DE">Delaware</option>
+					<option value="DC">District of Columbia</option>
+					<option value="FL">Florida</option>
+					<option value="GA">Georgia</option>
+					<option value="HI">Hawaii</option>
+					<option value="ID">Idaho</option>
+					<option value="IL">Illinois</option>
+					<option value="IN">Indiana</option>
+					<option value="IA">Iowa</option>
+					<option value="KS">Kansas</option>
+					<option value="KY">Kentucky</option>
+					<option value="LA">Louisiana</option>
+					<option value="ME">Maine</option>
+					<option value="MD">Maryland</option>
+					<option value="MA">Massachusetts</option>
+					<option value="MI">Michigan</option>
+					<option value="MN">Minnesota</option>
+					<option value="MS">Mississippi</option>
+					<option value="MO">Missouri</option>
+					<option value="MT">Montana</option>
+					<option value="NE">Nebraska</option>
+					<option value="NV">Nevada</option>
+					<option value="NH">New Hampshire</option>
+					<option value="NJ">New Jersey</option>
+					<option value="NM">New Mexico</option>
+					<option value="NY">New York</option>
+					<option value="NC">North Carolina</option>
+					<option value="ND">North Dakota</option>
+					<option value="OH">Ohio</option>
+					<option value="OK">Oklahoma</option>
+					<option value="OR">Oregon</option>
+					<option value="PA">Pennsylvania</option>
+					<option value="RI">Rhode Island</option>
+					<option value="SC">South Carolina</option>
+					<option value="SD">South Dakota</option>
+					<option value="TN">Tennessee</option>
+					<option value="TX">Texas</option>
+					<option value="UT">Utah</option>
+					<option value="VT">Vermont</option>
+					<option value="VA">Virginia</option>
+					<option value="WA">Washington</option>
+					<option value="WV">West Virginia</option>
+					<option value="WI">Wisconsin</option>
+					<option value="WY">Wyoming</option>
+				</select>
+				<input
+					onChange={props.handleInputChange}
+					value={props.city}
+					name="city"
+					type="text"
+					className="form-control"
+					placeholder="Enter city"
+					id="city"
+				/>
+				<br />
+				<button onClick={props.handleFormSubmit} className="btn btn-primary">
+					Search
+				</button>
+			</div>
+		</form>
+	);
 }
 
 export default SearchForm;
