2198e364be7719792566db8daa3f9f2e475ffc12
Alex Fernandes <alexfernands@outlook.com>
2021-02-09 16:15:06 -0500

    added more info API to cards

--- client/src/components/PropertyDetail.js
+++ client/src/components/PropertyDetail.js
@@ -6,10 +6,8 @@ import CardContent from "@material-ui/core/CardContent";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import Button from "@material-ui/core/Button";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-// import IconButton from "@material-ui/icons/IconButton";
 
 import { makeStyles } from "@material-ui/core/styles";
-// import zIndex from "@material-ui/core/styles/zIndex";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -49,11 +47,13 @@ function PropertyDetail({ property }) {
 				</CardActionArea>
 
 				<CardActions>
-					<Button variant="outlined" color="primary">
-						Contact Agent
+					<Button
+						href={property.rdc_web_url}
+						variant="outlined"
+						color="primary"
+					>
+						More Info
 					</Button>
-					
-
 					<FavoriteBorderIcon
 						className={classes.favoriteIcon}
 						href="#contained-buttons"
