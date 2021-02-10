4a58f563f409359fc7b0759f7e5bd71bb3085b70
Anitta29 <dananiuta@gmail.com>
2021-02-09 18:47:56 -0600

    mine

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Listings from './pages/Listings';
+import Home from './pages/Home';
+import NavBar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <React.Fragment>
+     <Router>
+       <NavBar/>
+       <Switch>
+<Route path="/listings" component={Listings}></Route>
+<Route path="/" component={Home}></Route>       
+       </Switch>
+       {/* <Footer/> */}
+
+       
+
+
+     </Router>
+   </React.Fragment>
   );
 }
 
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+
+const NavBar = () => {
+    return (
+        <ul>
+            <li>
+<NavLink to="/">
+Home
+</NavLink>
+            </li>
+            <li>
+<NavLink to="/listings">
+Listings
+</NavLink>
+            </li>
+        </ul>
+    )
+}
+
+export default NavBar
