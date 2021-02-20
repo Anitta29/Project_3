d09bcd5f33db1825726f78b9e029e14c2a6ebb38
Alex Fernandes <62030201+aafernands@users.noreply.github.com>
2021-02-20 17:57:16 -0500

    updated PropertyDetail.js cards

--- client/src/components/PropertyDetail.js
+++ client/src/components/PropertyDetail.js
@@ -12,13 +12,27 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
 	root: {
 		maxWidth: 345,
+		height: 450,
+
+		backgroundColor: "#312450",
+		color: "white",
+		size: 10,
+		fontFamily: "",
 	},
 	media: {
 		height: 140,
+		color: "white",
 	},
 	favoriteIcon: {
 		alignItems: "right",
 		cursor: "pointer",
+		color: "white",
+	},
+	Button: {
+		color: "white",
+	},
+	propertyDetails: {
+		marging: 0,
 	},
 }));
 
@@ -27,44 +41,41 @@ function PropertyDetail({ property, onFavoriteClick }) {
 	const classes = useStyles();
 
 	return (
-		<div className="text-center">
-			<Card className={classes.root}>
-				<CardActionArea>
-					<CardMedia className={classes.media} image={property.thumbnail} />
-					<CardContent>
-						<h3>Beds: {property.beds}</h3>
-						<h3>Baths: {property.baths}</h3>
-						<h3>
-							Price: $
-							{property.price
-								.toFixed()
-								.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")}
-						</h3>
-						<h3>Sqft: {property.lot_size?.size || 0} sqft</h3>
-						<h3>
-							Address: {property.address.line}, {property.address.city},{" "}
-							{property.address.state_code} {property.postal_code}
-						</h3>
-					</CardContent>
-				</CardActionArea>
+		<Card align="center" className={classes.root}>
+			<CardActionArea>
+				<CardMedia className={classes.media} image={property.thumbnail} />
+				<CardContent>
+					<h3 >Beds: {property.beds}</h3>
+					<h3>Baths: {property.baths}</h3>
+					<h3>
+						Price: $
+						{property.price.toFixed().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")}
+					</h3>
+					<h3>Sqft: {property.lot_size?.size || 0} sqft</h3>
+					<h3>
+						Address: {property.address.line}, {property.address.city},{" "}
+						{property.address.state_code} {property.postal_code}
+					</h3>
+				</CardContent>
+			</CardActionArea>
 
-				<CardActions>
-					<Button
-						href={property.rdc_web_url}
-						variant="outlined"
-						color="primary"
-					>
-						More Info
-					</Button>
-					<FavoriteBorderIcon
-						className={classes.favoriteIcon}
-						size="small"
-						color="primary"
-						onClick={() => onFavoriteClick(property)}
-					></FavoriteBorderIcon>
-				</CardActions>
-			</Card>
-		</div>
+			<CardActions>
+				<Button
+					className={classes.Button}
+					href={property.rdc_web_url}
+					variant="outlined"
+				>
+					More Info
+				</Button>
+
+				<FavoriteBorderIcon
+					className={classes.favoriteIcon}
+					size="small"
+					color="primary"
+					onClick={() => onFavoriteClick(property)}
+				></FavoriteBorderIcon>
+			</CardActions>
+		</Card>
 	);
 }
 
