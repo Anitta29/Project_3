89b11f5f07616d06a5d2fd7328e9f2e934316a71
Alex Fernandes <62030201+aafernands@users.noreply.github.com>
2021-03-01 18:46:40 -0500

    fux bug

--- server/server.js
+++ server/server.js
@@ -13,6 +13,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
+
+
 // Add routes, both API and view
 // app.use(routes);
 app.use(authRoutes);
