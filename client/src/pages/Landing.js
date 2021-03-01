9c469a8bfbb9d5b6f383aa7089d68b3039b8c153
Alex Fernandes <62030201+aafernands@users.noreply.github.com>
2021-03-01 16:59:05 -0500

    redesigned landing page structure

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-// import Container from "../components/Container";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
 import SearchArea from "../components/SearchArea";
 import API from "../utils/API";
 import PropertyDetail from "../components/PropertyDetail";
@@ -11,23 +9,31 @@ import Container from "@material-ui/core/Container";
 import { useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { setFavorite, removeFavorite } from "../redux/auth";
+import MyCarousel from "../components/Carousel/MyCarousel";
 
-// import UserLogin from "../components/UserLogin";
+import "../App.css";
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		flexGrow: 1,
+		position: "relative",
 	},
-	paper: {
-		// padding: theme.spacing(2),
-		textAlign: "center",
-		color: "white",
-		backgroundColor: "#312450",
+	carousel: {
+		position: "relative",
+		marginBottom: 50,
+	},
+	searchArea: {
+		position: "absolute",
+		top: "45%",
+		width: "100%",
+		zIndex: "3",
 	},
 	welcomeMessage: {
-		textAlign: "left",
-		marginLeft: "-11rem",
-		marginTop: "3rem",
+		color: "white",
+		marginBotton: 20,
+		fontFamily:
+			"Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+		fontWeight: "bold",
 	},
 }));
 function Landing() {
@@ -37,9 +43,9 @@ function Landing() {
 		city: "Manhattan",
 		result: [],
 	});
-	const history = useHistory()
-	const dispatch = useDispatch()
-	const { authenticated } = useSelector((state) => state.auth)
+	const history = useHistory();
+	const dispatch = useDispatch();
+	const { authenticated } = useSelector((state) => state.auth);
 
 	const handleInputChange = (event) => {
 		const { value, name } = event.target;
@@ -69,17 +75,17 @@ function Landing() {
 
 	const handleBtnClick = (property, isRemoved) => {
 		if (authenticated === false) {
-			return history.push("/login")
+			return history.push("/login");
 		}
 		if (isRemoved) {
 			return API.deleteProperty(property.listing_id).then((result) => {
-				alert("Property Removed!")
-				dispatch(removeFavorite(property.listing_id))
-			})
+				alert("Property Removed!");
+				dispatch(removeFavorite(property.listing_id));
+			});
 		}
 		API.saveProperty(property).then((result) => {
-			alert("Property Saved!")
-			dispatch(setFavorite(property.listing_id))
+			alert("Property Saved!");
+			dispatch(setFavorite(property.listing_id));
 		});
 	};
 
@@ -98,37 +104,43 @@ function Landing() {
 
 	return (
 		<Container align="center" maxWidth={false}>
-
 			<section className="display">
-				<Grid container spacing={3}>
-					<Grid item xs={12}>
-						<Paper className={classes.paper}>
-							<Typography
-								variant="h4"
-								gutterBottom
-								className={classes.welcomeMessage}
-							>
-								"Welcome to WOW Realtor! <br></br>To start select the desired
-								location above."
-							</Typography>
-							<SearchArea
-								state={search.state}
-								city={search.city}
-								handleInputChange={handleInputChange}
-								handleFormSubmit={handleFormSubmit}
-							/>
-						</Paper>
-					</Grid>
+				<div className={classes.root}>
+					<Grid container spacing={3}>
+						<Grid item xs={12}>
+							<div className={classes.carousel}>
+								<MyCarousel />
+								<div className={classes.searchArea}>
+									<Typography
+										variant="h4"
+										gutterBottom
+										className={classes.welcomeMessage}
+									>
+										"Welcome to WOW Realtor! <br></br>To start select the
+										desired location above."
+									</Typography>
+									<SearchArea
+										state={search.state}
+										city={search.city}
+										handleInputChange={handleInputChange}
+										handleFormSubmit={handleFormSubmit}
+									/>
+								</div>
+							</div>
 
-					{search.result.map((property, index) => (
-						<Grid key={index} item xs={12} md={6} lg={3}>
-							<PropertyDetail
-								property={property}
-								onClick={handleBtnClick}
-							/>
+							<Grid container spacing={3}>
+								{search.result.map((property, index) => (
+									<Grid key={index} item xs={12} md={6} lg={3}>
+										<PropertyDetail
+											property={property}
+											onClick={handleBtnClick}
+										/>
+									</Grid>
+								))}
+							</Grid>
 						</Grid>
-					))}
-				</Grid>
+					</Grid>
+				</div>
 			</section>
 		</Container>
 	);
